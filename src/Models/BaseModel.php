ed816e66f67c32ccf204580fb8ff3cc26323ee8f
Sagar Vaghela <71371324+sagar-vaghela97@users.noreply.github.com>
2022-03-15 18:26:48 +0530

    Add connection name in Base Model

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class BaseModel extends Model
 {
+    
+    /**
+     * Connection name
+     *
+     * @var string
+     */
+    protected $connection = 'mysql';
 
     /**
      *
